fbf9aeba649a5f283950ab8c221d4819d5355946
Louis Sch <private@louissch.com>
2025-06-02 22:10:35 +0200

    A

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from app.predictor import run_prediction  # You'll define this in predictor.py
+from predictor import run_prediction  # You'll define this in predictor.py
 
 st.title("Smart Prediction Wizard")
 uploaded_file = st.file_uploader("Upload your CSV or text file")
